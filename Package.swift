bbdc3bacd76b930676ae0bec33703b1c3720347b
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-02 15:15:51 +0300

    added Package.swift

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "adrevenue-apple-sdk",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "adrevenue-apple-sdk",
+            targets: ["adrevenue-apple-sdk"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework-Static.git",  .exact("6.12.0"))
+        ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "adrevenue-apple-sdk"),
+            dependencies: [ 
+                            .product(name: "AppsFlyerLib-Static", package: "AppsFlyerFramework-Static")],
+        path: "Sources/"
+        ),
+        .testTarget(
+            name: "adrevenue-apple-sdkTests",
+            dependencies: ["adrevenue-apple-sdk"]),
+    ]
+)
