1b2a542934c6bec4587827204646521b55bc9337
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-06 12:28:29 +0300

    add AppsflyerFrame work as a binary

--- Package.swift
+++ Package.swift
@@ -20,5 +20,10 @@ let package = Package(
             name: "adrevenue-apple-sdk",
             path: "iOS/AppsFlyerAdRevenue.xcframework"
         )
+        .binaryTarget(
+            name: "AppsFlyerLib",
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.12.1/AppsFlyerLib.xcframework.zip",
+            checksum: "5d418a84ec708e6142f4eb228d6163c0693559c31a67b741e4dd816fb20f662e"
+        )
     ]
 )
