4a5ca842f15722c52ba7f670546dcaf2851373fc
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-06 11:55:31 +0300

    .

--- Package.swift
+++ Package.swift
@@ -16,9 +16,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
+        .binaryTarget(
             name: "adrevenue-apple-sdk",
-            dependencies: [ .product(name: "AppsFlyerFramework", package: "AppsFlyerFramework")], 
             path: "iOS/AppsFlyerAdRevenue.xcframework"
         ),
         .testTarget(
