bd82ad00855188b5f3fcae0168a07a6caa68f420
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-06 14:09:28 +0300

    added AppsflyerLib

--- Package.swift
+++ Package.swift
@@ -18,6 +18,11 @@ let package = Package(
         .binaryTarget(
             name: "adrevenue-apple-sdk",
             path: "iOS/AppsFlyerAdRevenue.xcframework"
+        ),
+        .binaryTarget(
+            name: "AppsFlyerLib",
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.12.1/AppsFlyerLib.xcframework.zip",
+            checksum: "5d418a84ec708e6142f4eb228d6163c0693559c31a67b741e4dd816fb20f662e"
         )
     ]
 )
