2cc4621e2a1ddbd2d8bbf1c13995015858da7527
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-06 12:30:35 +0300

    removed dependency

--- Package.swift
+++ Package.swift
@@ -11,8 +11,6 @@ let package = Package(
             name: "adrevenue-apple-sdk",
             targets: ["adrevenue-apple-sdk"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.12.0")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
