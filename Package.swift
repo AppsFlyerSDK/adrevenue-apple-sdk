9a297eb46d8b11b10389f20796a6ceefdee8b2d9
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-06 12:37:13 +0300

    syntax

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "adrevenue-apple-sdk",
-            dependencies: "AppsFlyerFramework",
+            dependencies: ["AppsFlyerFramework"],
             path: "iOS/AppsFlyerAdRevenue.xcframework"
         )
     ]
