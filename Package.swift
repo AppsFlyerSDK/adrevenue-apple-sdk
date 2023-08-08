29b11c3820dc70964a8c3e9736543f4d28dccf1a
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-08 12:48:21 +0300

    syntax

--- Package.swift
+++ Package.swift
@@ -23,12 +23,12 @@ let package = Package(
         .binaryTarget(
             name: "adrevenue-apple-sdk-BT",
             path: "iOS/AppsFlyerAdRevenue.xcframework"
-        )
+        ),
         .binaryTarget(
             name: "AppsFlyerLib",
             url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.12.1/AppsFlyerLib.xcframework.zip",
             checksum: "5d418a84ec708e6142f4eb228d6163c0693559c31a67b741e4dd816fb20f662e"
-        )
+        ),
         .target(name: "adrevenue-apple-sdk2",
                 dependencies: [
                     .target(name: "adrevenue-apple-sdk-BT"),
