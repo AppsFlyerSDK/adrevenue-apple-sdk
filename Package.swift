3df8859e701b6f90903794ac1e371c08a326f134
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-08 12:38:26 +0300

    try new solution

--- Package.swift
+++ Package.swift
@@ -24,5 +24,17 @@ let package = Package(
             name: "adrevenue-apple-sdk",
             path: "iOS/AppsFlyerAdRevenue.xcframework"
         )
+        .binaryTarget(
+            name: "AppsFlyerLib",
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.12.1/AppsFlyerLib.xcframework.zip",
+            checksum: "5d418a84ec708e6142f4eb228d6163c0693559c31a67b741e4dd816fb20f662e"
+        )
+        .target(name: "adrevenue-apple-sdk2",
+                dependencies: [
+                    .target(name: "adrevenue-apple-sdk"),
+                    .target(name: "AppsFlyerLib")
+                ],
+                path: "FrameworkCTargets"
+        )
     ]
 )
