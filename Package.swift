f564756f8def42abcff4bb8a443a2e4da0d9edc7
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-09 14:23:02 +0300

    only adrevenue is part of the package - AppsFlyerLib is excluded.
    this was decided after discussing it in the scrum.

--- Package.swift
+++ Package.swift
@@ -6,30 +6,15 @@ import PackageDescription
 let package = Package(
     name: "adrevenue-apple-sdk",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "adrevenue-apple-sdk",
-            targets: ["adrevenue-appsflyer"])
-    ],
-
-    dependencies: [
-        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework-Static", from: "6.12.1"),
+            targets: ["adrevenue-apple-sdk"])
     ],
 
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "adrevenue-apple-sdk",
             path: "iOS/AppsFlyerAdRevenue.xcframework"
-        ),
-        .target(
-            name: "adrevenue-appsflyer",
-            dependencies: [
-                .product(name: "AppsFlyerLib-Static", package: "AppsFlyerFramework-Static"),
-                .target(name: "adrevenue-apple-sdk")
-                ],
-            path: "adrevenue-appsflyer"
         )
     ]
 )
