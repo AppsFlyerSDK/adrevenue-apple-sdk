bed55ca35db48b8e7c57c01534192e032f04f198
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-13 10:45:50 +0300

    purchase connector not working either on 14.2 - so maybe this shouldn't either.

--- Package.swift
+++ Package.swift
@@ -1,20 +1,17 @@
-// swift-tools-version: 5.3
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "adrevenue-apple-sdk",
-    platforms: [
-        .iOS(.v11)
-    ],
     products: [
         .library(
             name: "adrevenue-apple-sdk",
             targets: ["adrevenue-apple-sdk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.10.1")
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework-Static", from: "6.12.1"),
     ],
     targets: [
         .binaryTarget(
