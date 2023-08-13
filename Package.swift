c34cfc81f5a930c4b0ed8789218c40940d726518
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-13 10:30:57 +0300

    to 5.3

--- Package.swift
+++ Package.swift
@@ -1,25 +1,35 @@
+// swift-tools-version: 5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// swift-tools-version:5.3
 import PackageDescription
 
-
 let package = Package(
-    name: "MyLibrary",
+    name: "adrevenue-apple-sdk",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary", "adrevenue-apple-sdk"])
+            name: "adrevenue-apple-sdk",
+            targets: ["adrevenue-appsflyer"])
     ],
+
+    dependencies: [
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework-Static", from: "6.12.1"),
+    ],
+
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyLibrary"
-        ),
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "adrevenue-apple-sdk",
-            path: "iOS/AppsFlyerAdRevenue.xcframework"
+            path: "iOS/AppsFlyerAdRevenue.xcframework/ios-arm64_armv7/AppsFlyerAdRevenue.framework"
+        ),
+        .target(
+            name: "adrevenue-appsflyer",
+            dependencies: [
+                .product(name: "AppsFlyerLib-Static", package: "AppsFlyerFramework-Static"),
+                .target(name: "adrevenue-apple-sdk")
+                ],
+            path: "adrevenue-appsflyer"
         )
     ]
 )
