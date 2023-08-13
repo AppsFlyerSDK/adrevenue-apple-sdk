9b4c1798ea5429a5738e898a850cd0abfdba3921
Moris Gateno <moris.gateno@appsflyer.com>
2023-08-13 10:39:35 +0300

    .

--- Package.swift
+++ Package.swift
@@ -1,14 +1,20 @@
-// swift-tools-version: 5.4
+// swift-tools-version: 5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "adrevenue-apple-sdk",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         .library(
             name: "adrevenue-apple-sdk",
             targets: ["adrevenue-apple-sdk"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.10.1")
     ],
     targets: [
         .binaryTarget(
